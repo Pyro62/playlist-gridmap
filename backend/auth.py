f6226ae896c163c0736b021726410a60b23c6b6b
PieRoe <ethanluu0508@gmail.com>
2026-07-06 03:53:03 -0700

    track catalog hotfix 3 fixed scope

--- backend/auth.py
+++ backend/auth.py
@@ -83,7 +83,7 @@ def generateRandomString(length: int):
 @router.get('/auth/login')
 async def login():
     state = generateRandomString(16)
-    scope = 'user-read-email user-top-read'
+    scope = 'user-read-email user-top-read playlist-read-private playlist-read-collaborative'
     
     params = {
         'client_id': SPOTIFY_CLIENT_ID,
